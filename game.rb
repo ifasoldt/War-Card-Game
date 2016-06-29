675383b9d1950549d764bfed1f649eec9e2cc5b6
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-06-29 19:53:14 -0400

    4 card wars work

--- game.rb
+++ game.rb
@@ -3,44 +3,57 @@ require './deck'
 require './card'
 
 class Game
-  attr_accessor :round_counter, :war_counter, :war_box, :player1, :player2,
+  attr_accessor :round_counter, :war_counter, :war_box, :player1, :player2
   def initialize
     self.round_counter = 0
     self.war_counter = 0
     self.player1 = Player.new("Sean")
     self.player2 = Player.new("Isaiah")
+    self.war_box = []
+    self.total_counter = []
   end
 
-  def play
-    puts "All right, lets play war!"
-    isaiah.shuffle
-    sean.shuffle
-    until sean.playing_deck == [] || isaiah.playing_deck == []
-      sean.play_card
-      isaiah.play_card
-      if sean.played_card.value > isaiah.played_card.value
-        sean.capture_deck << sean.played_card && isaiah.played_card && war_box
-        puts "Sean's #{sean.played_card.name} of #{sean.played_card.suit} beat Isaiah's #{isaiah.played_card.name} of #{isaiah.played_card.suit}"
-        @round_counter += 1
-      elsif isaiah.played_card.value > sean.played_card.value
-        isaiah.capture_deck << isaiah.played_card && isaiah.played_card && war_box
-        @round_counter += 1
-        puts "Isaiah's #{isaiah.played_card.name} of #{isaiah.played_card.suit} beat Sean's #{sean.played_card.name} of #{sean.played_card.suit}"
-      else
-        war_time
-        puts "War!!"
+    def play
+      puts "All right, lets play war!"
+      player2.shuffle
+      player1.shuffle
+      until player1.playing_deck == [] || player2.playing_deck == []
+        player1.play_card
+        player2.play_card
+        combine_decks
+        if player1.played_card.value > player2.played_card.value
+          player1.capture_deck.insert(0, player2.played_card, player1.played_card)
+          player1.capture_deck += war_box
+          war_box.clear
+          puts "Sean's #{player1.played_card.name} of #{player1.played_card.suit} beat Isaiah's #{player2.played_card.name} of #{player2.played_card.suit}"
+          puts player1.capture_deck.length
+          @round_counter += 1
+        elsif player2.played_card.value > player1.played_card.value
+          player2.capture_deck.insert(0, player2.played_card, player1.played_card)
+          player2.capture_deck += war_box
+          war_box.clear
+          puts "Isaiah's #{player2.played_card.name} of #{player2.played_card.suit} beat Sean's #{player1.played_card.name} of #{player1.played_card.suit}"
+          puts player2.capture_deck.length
+          @round_counter += 1
+        else
+          war_time
+          puts "War!!"
+        end
+        combine_decks
       end
-      combine_decks
+      if player1.capture_deck.length == 0 && player1.playing_deck.length == 0
+        puts "Isaiah won this game after #{self.round_counter} rounds and survived #{self.war_counter} WARs. Would you like a rematch? (Y/N)"
+        puts player2.capture_deck.length + player2.playing_deck.length
+      elsif player2.capture_deck.length == 0 && player2.playing_deck.length == 0
+        puts "Sean won this game after #{self.round_counter} rounds and survived #{self.war_counter} WARs. Would you like a rematch? (Y/N)"
+          puts player1.capture_deck.length + player1.playing_deck.length
+      else
+        puts "it's a tie! Would you like a rematch"
+      end
+      ask_for_rematch
+
+      # self.total_counter << { rounds: @rounds}
     end
-    if sean.capture_deck.length > isaiah.capture_deck.length
-      puts "Sean won this game after #{self.round_counter} rounds and survived #{self.war_counter} WARs. Would you like a rematch? (Y/N)"
-    elsif isaiah.capture_deck.length > sean.capture_deck.length
-      puts "Isaiah won this game after #{self.round_counter} rounds and survived #{self.war_counter} WARs. Would you like a rematch? (Y/N)"
-    else
-      puts "it's a tie! Would you like a rematch"
-    end
-    ask_for_rematch
-  end
 
   def ask_for_rematch
     resp = gets.chomp&.downcase[0]
@@ -51,23 +64,25 @@ class Game
     end
   end
   def combine_decks
-    if sean.playing_deck == []
-      sean.playing_deck = sean.capture_deck.shuffle!
-      sean.capture_deck = []
+    if player1.playing_deck == []
+      player1.playing_deck = player1.capture_deck.shuffle!
+      player1.capture_deck = []
     end
-    if isaiah.playing_deck == []
-      isaiah.playing_deck = isaiah.capture_deck.shuffle!
-      isaiah.capture_deck = []
+    if player2.playing_deck == []
+      player2.playing_deck = player2.capture_deck.shuffle!
+      player2.capture_deck = []
     end
   end
 
   def war_time
-    war_box << isaiah.played_card && sean.played_card
-    4.times.do |x|
-    combine_decks
-    sean.play_card
-    isaiah.play_card
-    war_box << isaiah.played_card && sean.played_card
+    war_box.insert(-1, player2.played_card, player1.played_card)
+    4.times do |x|
+      combine_decks
+      player1.play_card
+      player2.play_card
+      war_box.insert(-1, player2.played_card, player1.played_card)
+    end
+      @war_counter += 1
   end
 end
 
