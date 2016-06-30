57e1c8616c1bb03311fb3d19d6ec4a7b7b676ec7
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-06-29 20:29:47 -0400

    game now keeps total records until program exited

--- game.rb
+++ game.rb
@@ -3,18 +3,27 @@ require './deck'
 require './card'
 
 class Game
-  attr_accessor :round_counter, :war_counter, :war_box, :player1, :player2
+  attr_accessor :round_counter, :war_counter, :war_box, :player1, :player2, :winner, :total_record
   def initialize
     self.round_counter = 0
     self.war_counter = 0
     self.player1 = Player.new("Sean")
     self.player2 = Player.new("Isaiah")
     self.war_box = []
-    self.total_counter = []
+    self.winner = ""
+    self.total_record = []
   end
 
-    def play
+
+  def play
+    rematch = true
+    while rematch
       puts "All right, lets play war!"
+      player1.playing_deck = Deck.new.deck
+      player2.playing_deck = Deck.new.deck
+      war_box.clear
+      player1.capture_deck.clear
+      player2.capture_deck.clear
       player2.shuffle
       player1.shuffle
       until player1.playing_deck == [] || player2.playing_deck == []
@@ -44,25 +53,34 @@ class Game
       if player1.capture_deck.length == 0 && player1.playing_deck.length == 0
         puts "Isaiah won this game after #{self.round_counter} rounds and survived #{self.war_counter} WARs. Would you like a rematch? (Y/N)"
         puts player2.capture_deck.length + player2.playing_deck.length
+        @winner = "Isaiah"
       elsif player2.capture_deck.length == 0 && player2.playing_deck.length == 0
         puts "Sean won this game after #{self.round_counter} rounds and survived #{self.war_counter} WARs. Would you like a rematch? (Y/N)"
-          puts player1.capture_deck.length + player1.playing_deck.length
+        puts player1.capture_deck.length + player1.playing_deck.length
+        @winner = "Sean"
       else
         puts "it's a tie! Would you like a rematch"
+        @winner = "Tie!"
       end
-      ask_for_rematch
-
-      # self.total_counter << { rounds: @rounds}
-    end
-
-  def ask_for_rematch
-    resp = gets.chomp&.downcase[0]
-    if resp == "y"
-      Game.new.play
-    else
-      puts "Thanks for playing."
+      # ask_for_rematch
+      self.total_record << { rounds: @round_counter, Wars: @war_counter, winner: @winner }
+      @round_counter = 0
+      @war_counter = 0
+      @winner = ''
+      puts "Do you want a rematch? (Y/N)"
+      response = gets.chomp&.downcase[0]
+      rematch = response == "y" ? true : false
     end
   end
+
+  # def ask_for_rematch
+  #   resp = gets.chomp&.downcase[0]
+  #   if resp == "y"
+  #     Game.new.play
+  #   else
+  #     puts "Thanks for playing."
+  #   end
+  # end
   def combine_decks
     if player1.playing_deck == []
       player1.playing_deck = player1.capture_deck.shuffle!
@@ -84,6 +102,12 @@ class Game
     end
       @war_counter += 1
   end
+
+  def results_to_file
+    puts self.total_record.inspect
+  end
 end
 
-Game.new.play
+bob = Game.new
+bob.play
+bob.results_to_file
